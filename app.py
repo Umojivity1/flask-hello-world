41d9652e652c239faff7377009e0893f0fb8b59b
Umojivity1 <admin@umojivity.com>
2015-08-02 16:11:48 +0000

    added search query route to app

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+# dynamic routes
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
